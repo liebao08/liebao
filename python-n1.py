59240847d7d6bf462625914e5c6635073c1fee17
liebao08 <liebao08@example.com>
2020-07-28 14:50:45 +0200

    add python-n1.py

---
+++ python-n1.py
@@ -0,0 +1,32 @@
+print("2" + "3")
+
+print(int("2") + int("3"))
+
+a = 7
+print(7)
+
+print(a + 3)
+
+b = 123.456
+print(b)
+
+c = "This is a string"
+print(c)
+
+foo = input("Enter a number")
+
+print(foo)
+
+d = 3
+d *= 4
+print(d)
+
+e = 7
+e = e + 2
+
+my_boolean = True
+my_boolean
+
+2 == 3
+
+"hello gio" == "hello gio"
